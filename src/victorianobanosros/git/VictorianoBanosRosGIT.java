60139bca2d67f47cea3f4b99eb137dd2578a504d
VICTOR <VICTOR@DESKTOP-FPVB0MP>
2023-03-06 23:08:49 +0100

    cambio variable a 888

--- src/victorianobanosros/git/VictorianoBanosRosGIT.java
+++ src/victorianobanosros/git/VictorianoBanosRosGIT.java
@@ -16,7 +16,7 @@ public class VictorianoBanosRosGIT {
     public static void main(String[] args) {
         // TODO code application logic here
     
-    int var=7;
+    int var=888;
     
     System.out.println("Variable= "+var);
     
