239d00f7906fddc0ddfc0a6d9e2d84442c689e8d
VICTOR <VICTOR@DESKTOP-FPVB0MP>
2023-03-06 12:19:47 +0100

    Variable inicial. Variable=1

---
+++ src/victorianobanosros/git/VictorianoBanosRosGIT.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package victorianobanosros.git;
+
+/**
+ *
+ * @author VICTOR
+ */
+public class VictorianoBanosRosGIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    int var=1;
+    
+    System.out.println("Variable= "+var);
+    
+    }
+    
+}
