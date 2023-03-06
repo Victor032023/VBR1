33a88b2f1bb55e8b939439a5759f3ac39cbf992d
VICTOR <VICTOR@DESKTOP-FPVB0MP>
2023-03-06 12:30:06 +0100

    Variable=2

--- src/victorianobanosros/git/VictorianoBanosRosGIT.java
+++ src/victorianobanosros/git/VictorianoBanosRosGIT.java
@@ -16,7 +16,7 @@ public class VictorianoBanosRosGIT {
     public static void main(String[] args) {
         // TODO code application logic here
     
-    int var=1;
+    int var=2;
     
     System.out.println("Variable= "+var);
     
